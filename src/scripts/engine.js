3908c3a9b91558413bbe9fce0954d5f3d9e082c4
julio gabriel <juliogabriel78956@gmail.com>
2025-09-10 18:05:47 -0300

    Ralph

---
+++ src/scripts/engine.js
@@ -0,0 +1,30 @@
+const state = {
+  view: {
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector("#enemy"),
+    timeLeft:document.querySelector("#time-left"),
+    score:document.querySelector("#score"),
+  },
+  values: {},
+};
+
+function randomSquare() {
+    state.view.forEach((square) =>{
+        square.classList.remove("enemy");
+ })
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy")
+}
+
+
+function addListenerHitBox(params) {
+    state.view.squares.forEach((square) =>  {})
+
+}
+
+function init() {
+    
+    randomSquare()
+}
+init();
